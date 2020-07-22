d3f02525e75cb3b6bfcba9b04e920891ecfdcdde
ArunTeltia <teltia.arun18@gmail.com>
2020-07-22 19:42:15 +0530

    status bar

--- models/User.js
+++ models/User.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
+    
     email: {
         type: String,
         required: true,
         unique: true,
     },
+    phone:{
+        type:Number,
+        required: true,
+    },
+    
     password: {
         type: String,
         required: true,
